d42ecb3fdf7c06ddd96d0124b51ead3f783af50d
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-08-07 17:45:20 +0200

    [api] Added tests for accounting view

--- django_freeradius/tests/test_api.py
+++ django_freeradius/tests/test_api.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 RadiusPostAuth = swapper.load_model("django_freeradius", "RadiusPostAuth")
+RadiusAccounting = swapper.load_model("django_freeradius", "RadiusAccounting")
 
 
 class TestApi(TestCase):
@@ -52,3 +53,67 @@ class TestApi(TestCase):
         response = self.client.post(reverse('freeradius:postauth'), {})
         self.assertEqual(RadiusPostAuth.objects.all().count(), 0)
         self.assertEqual(response.status_code, 400)
+
+
+    def test_accounting_start(self):
+        RadiusAccounting.objects.all().count()
+        self.assertEqual(RadiusAccounting.objects.all().count(), 0)
+        response = self.client.post(reverse('freeradius:accounting'),
+                                    {'username': 'admin', 'NAS-IP-Address': '172.16.64.91',
+                                     'NAS-Port': "1",  'Called-Station-Id': '00-27-22-F3-FA-F1:hostname',
+                                     'Calling-Station-Id': '5c:7d:c1:72:a7:3b',  'NAS-Identifier': '',
+                                     'Acct-Status-Type': 'Start', 'Acct-Authentic': 'RADIUS',
+                                     'Acct-Delay-Time': '0',
+                                     'Acct-Unique-Session-Id': '75058e505ac30c5b4b9c686ee853b70f',
+                                     'Acct-Terminate-Cause': 'User-Request',  'Acct-Input-Octets': '9900909',
+                                     'Acct-Output-Octets': '1511075509',  'NAS-Port-Type': 'Async',
+                                     'Acct-Session-Time': '261', 'Login-Service': 'Telnet',
+                                     'Login-IP-Host': '172.16.64.25', 'Acct-Session-Id': '35000006',
+                                     'Framed-Protocol': '', 'Framed-IP-Address': '',
+                                     'Service-Type': 'Login-User', 'Realm': '',
+                                     'Acct-Authentic': 'RADIUS'})
+        self.assertEqual(RadiusAccounting.objects.filter(session_id='35000006').count(), 1)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, {''})
+
+    def test_accounting_update(self):
+        RadiusAccounting.objects.all().count()
+        self.assertEqual(RadiusAccounting.objects.all().count(), 0)
+        response = self.client.post(reverse('freeradius:accounting'),
+                                    {'username': 'admin', 'NAS-IP-Address': '172.16.64.91',
+                                     'NAS-Port': "2",  'Called-Station-Id': '00-27-22-F3-FA-F1:hostname',
+                                     'Calling-Station-Id': '5c:7d:c1:72:a7:3b',  'NAS-Identifier': '',
+                                     'Acct-Status-Type': 'Interim-Update', 'Acct-Authentic': 'RADIUS',
+                                     'Acct-Delay-Time': '0',
+                                     'Acct-Unique-Session-Id': '75058e505ac30c5b4b9c686ee853b70f',
+                                     'Acct-Terminate-Cause': 'User-Request',  'Acct-Input-Octets': '9900909',
+                                     'Acct-Output-Octets': '15110755409',  'NAS-Port-Type': 'Async',
+                                     'Acct-Session-Time': '262', 'Login-Service': 'Telnet',
+                                     'Login-IP-Host': '172.16.64.25', 'Acct-Session-Id': '35000006',
+                                     'Framed-Protocol': '', 'Framed-IP-Address': '',
+                                     'Service-Type': 'Login-User', 'Realm': '',
+                                     'Acct-Authentic': 'RADIUS'})
+        self.assertEqual(RadiusAccounting.objects.filter(session_id='35000006').count(), 1)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, {''})
+
+    def test_accounting_stop(self):
+        RadiusAccounting.objects.all().count()
+        self.assertEqual(RadiusAccounting.objects.all().count(), 0)
+        response = self.client.post(reverse('freeradius:accounting'),
+                                    {'username': 'admin', 'NAS-IP-Address': '172.16.64.91',
+                                     'NAS-Port': "1",  'Called-Station-Id': '00-27-22-F3-FA-F1:hostname',
+                                     'Calling-Station-Id': '5c:7d:c1:72:a7:3b',  'NAS-Identifier': '',
+                                     'Acct-Status-Type': 'Stop', 'Acct-Authentic': 'RADIUS',
+                                     'Acct-Delay-Time': '0',
+                                     'Acct-Unique-Session-Id': '75058e505ac30c5b4b9c686ee853b70f',
+                                     'Acct-Terminate-Cause': 'User-Request',  'Acct-Input-Octets': '9900909',
+                                     'Acct-Output-Octets': '1511075509',  'NAS-Port-Type': 'Async',
+                                     'Acct-Session-Time': '261', 'Login-Service': 'Telnet',
+                                     'Login-IP-Host': '172.16.64.25', 'Acct-Session-Id': '35000006',
+                                     'Framed-Protocol': '', 'Framed-IP-Address': '',
+                                     'Service-Type': 'Login-User', 'Realm': '',
+                                     'Acct-Authentic': 'RADIUS'})
+        self.assertEqual(RadiusAccounting.objects.filter(session_id='35000006').count(), 1)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, {''})
