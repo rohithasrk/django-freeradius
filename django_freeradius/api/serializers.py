35c7652bcb8eaad00ed0b1943aca606f8d807d56
Fiorella De Luca <deluca_fiorella@yahoo.it>
2017-08-14 15:24:40 +0200

    [api] errors

--- django_freeradius/api/serializers.py
+++ django_freeradius/api/serializers.py
@@ -1,7 +1,9 @@
 import swapper
 from rest_framework import serializers
+from django.utils import timezone
 
 RadiusPostAuth = swapper.load_model("django_freeradius", "RadiusPostAuth")
+RadiusAccounting = swapper.load_model("django_freeradius", "RadiusAccounting")
 
 
 class RadiusPostAuthSerializer(serializers.ModelSerializer):
@@ -19,4 +21,34 @@ class RadiusPostAuthSerializer(serializers.ModelSerializer):
         model = RadiusPostAuth
         fields = ['username', 'password', 'reply',
                   'called_station_id',
+                  'calling_station_id']
+
+
+class RadiusAccountingSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if data['Acct-Status-Type'] == 'Interim-Update':
+            update_time = timezone.now()
+            data['update_time'] = 'update_time'
+        return data
+        if data['Acct-Status-Type'] == 'stop':
+            stop_time = timezone.now()
+            data['stop_time'] = 'stop_time'
+        return data
+        if data['session_time'] is None:
+            session_time = timezone.now() - start_time
+            data['session_time'] = 'session_time'
+        return data
+
+    class Meta:
+        model = RadiusAccounting
+        fields = ['session_id', 'unique_id',
+                  'username', 'start_time',
+                  'nas_ip_address', 'nas_port_id',
+                  'nas_port_type', 'session_time',
+                  'authentication', 'realm',
+                  'input_octets', 'output_octets',
+                  'called_station_id', 'terminate_cause',
+                  'service_type', 'framed_protocol',
+                  'framed_ip_address', 'groupname',
                   'calling_station_id']
--- django_freeradius/api/views.py
+++ django_freeradius/api/views.py
@@ -4,8 +4,10 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.utils import timezone
+from rest_framework.generics import UpdateAPIView
 
 from .serializers import RadiusPostAuthSerializer
+from .serializers import RadiusAccountingSerializer
 
 RadiusPostAuth = swapper.load_model("django_freeradius", "RadiusPostAuth")
 RadiusAccounting = swapper.load_model("django_freeradius", "RadiusAccounting")
@@ -41,70 +43,26 @@ class PostAuthView(generics.CreateAPIView):
 postauth = PostAuthView.as_view()
 
 
-@api_view(['POST'])
-def accounting(request):
-    session_id = request.data.get('Acct-Session-Id')
-    if RadiusAccounting.objects.filter(session_id=session_id).exists():
-        radacct = RadiusAccounting.objects.get(session_id=session_id)
-        radacct.username = request.data.get("username", '')
-        radacct.session_id = request.data.get('Acct-Session-Id', '')
-        radacct.groupname = request.data.get('groupname', '')
-        radacct.unique_id = request.data.get('Acct-Unique-Session-Id', '')
-        radacct.realm = request.data.get('Realm', '')
-        radacct.nas_ip_address = request.data.get('NAS-IP-Address', '')
-        radacct.nas_port_id = request.data.get('NAS-Port', '')
-        radacct.nas_port_type = request.data.get('NAS-Port-Type', '')
-        radacct.session_time = request.data.get('Acct-Session-Time', '')
-        radacct.authentication = request.data.get('Acct-Authentic', '')
-        radacct.input_octets = request.data.get('Acct-Input-Octets', '')
-        radacct.output_octets = request.data.get('Acct-Output-Octets', '')
-        radacct.calling_station_id = request.data.get('Calling-Station-Id', '')
-        radacct.called_station_id = request.data.get('Called-Station-Id', '')
-        radacct.terminate_cause = request.data.get('Acct-Terminate-Cause', '')
-        radacct.service_type = request.data.get('Service-Type', '')
-        radacct.framed_protocol = request.data.get('Framed-Protocol', '')
-        radacct.framed_ip_address = request.data.get('Framed-IP-Address', '')
-        status_type = request.data.get('Acct-Status-Type')
-        if status_type.lower() == 'interim-update':
-            update_time = timezone.now()
-            radacct.update_time = update_time
-        if status_type.lower() == 'stop':
-            stop_time = timezone.now()
-            radacct.stop_time = stop_time
-        radacct.save()
-        return Response({''}, status=204)
-    else:
-        session_id = request.data.get('Acct-Session-Id')
-        unique_id = request.data.get('Acct-Unique-Session-Id')
-        username = request.data.get('username')
-        groupname = request.data.get('groupname')
-        realm = request.data.get('Realm')
-        session_time = request.data.get('Acct-Session-Time')
-        nas_ip_address = request.data.get('NAS-IP-Address')
-        nas_port_id = request.data.get('NAS-Port')
-        nas_port_type = request.data.get('NAS-Port-Type')
-        authentication = request.data.get('Acct-Authentic')
-        input_octets = request.data.get('Acct-Input-Octets')
-        output_octets = request.data.get('Acct-Output-Octets')
-        calling_station_id = request.data.get('Calling-Station-Id')
-        called_station_id = request.data.get('Called-Station-Id')
-        terminate_cause = request.data.get('Acct-Terminate-Cause')
-        service_type = request.data.get('Service-Type')
-        framed_protocol = request.data.get('Framed-Protocol')
-        framed_ip_address = request.data.get('Framed-IP-Address')
-        status_type = request.data.get('Acct-Status-Type')
-        start_time = timezone.now()
-        if session_time is None:
-            session_time = timezone.now() - start_time
-        RadiusAccounting.objects.create(session_id=session_id, unique_id=unique_id,
-                                        username=username, start_time=start_time,
-                                        nas_ip_address=nas_ip_address, nas_port_id=nas_port_id,
-                                        nas_port_type=nas_port_type,
-                                        session_time=session_time,
-                                        authentication=authentication, realm=realm,
-                                        input_octets=input_octets,
-                                        output_octets=output_octets, calling_station_id=calling_station_id,
-                                        called_station_id=called_station_id, terminate_cause=terminate_cause,
-                                        service_type=service_type, framed_protocol=framed_protocol,
-                                        framed_ip_address=framed_ip_address, groupname=groupname)
-    return Response({''}, status=204)
+class AccountingView(generics.ListCreateAPIView, generics.UpdateAPIView):
+    serializer_class = RadiusAccountingSerializer
+
+    def get_object(self):
+        try:
+            queryset = RadiusAccounting.objects.all()
+            session_id = self.request.query_params.get('session_id')
+            if queryset.filter(session_id=session_id).exists():
+              return queryset
+        except:
+            return None
+
+
+    def accounting(self, queryset, request, *args, **kwargs):
+        if get_object is None:
+           response = self.create(request, *args, **kwargs)
+           return response
+        else:
+            response = self.update(request, *args, **kwargs)
+            return response
+
+
+accounting = AccountingView.as_view()
